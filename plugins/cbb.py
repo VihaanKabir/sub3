8044586365aadec1afdff0caf8ebf3a032ee25c0
IndomieGorengSatu <96320576+IndomieGorengSatu@users.noreply.github.com>
2023-01-27 20:30:20 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -3,10 +3,21 @@
 # t.me/SharingUserbot & t.me/Lunatic0de
 
 from bot import Bot
+from config import OWNER
 from Data import Data
 from pyrogram import filters
 from pyrogram.errors import MessageNotModified
 from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, Message
+
+
+@Bot.on_message(filters.private & filters.incoming & filters.command("about"))
+async def _about(client: Bot, msg: Message):
+    await client.send_message(
+        msg.chat.id,
+        Data.ABOUT.format(client.username, OWNER),
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(Data.mbuttons),
+    )
 
 
 @Bot.on_message(filters.private & filters.incoming & filters.command("help"))
@@ -22,7 +33,16 @@ async def _help(client: Bot, msg: Message):
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
-    if data == "help":
+    if data == "about":
+        try:
+            await query.message.edit_text(
+                text=Data.ABOUT.format(client.username, OWNER),
+                disable_web_page_preview=True,
+                reply_markup=InlineKeyboardMarkup(Data.mbuttons),
+            )
+        except MessageNotModified:
+            pass
+    elif data == "help":
         try:
             await query.message.edit_text(
                 text="<b>Cara Menggunakan Bot ini</b>\n" + Data.HELP,
