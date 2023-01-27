c983c37b270c7d0c976168a81ab3dfe1f6953bde
IndomieGorengSatu <96320576+IndomieGorengSatu@users.noreply.github.com>
2023-01-27 20:27:27 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -8,6 +8,7 @@ class Data:
     HELP = """
 <b> ❏ Perintah untuk Pengguna BOT
  ├ /start - Mulai Bot
+ ├ /about - Tentang Bot ini
  ├ /help - Bantuan Perintah Bot ini
  ├ /ping - Untuk mengecek bot hidup
  └ /uptime - Untuk melihat status bot 
@@ -31,12 +32,23 @@ Managed by </b><a href='https://t.me/IndomieProject'>@IndomieProject</a>
 
     mbuttons = [
         [
+            InlineKeyboardButton("ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help"),
             InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close")
         ],
     ]
 
     buttons = [
         [
+            InlineKeyboardButton("ᴛᴇɴᴛᴀɴɢ sᴀʏᴀ", callback_data="about"),
             InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close")
         ],
     ]
+
+   ABOUT = """
+<b>Tentang Bot ini:
+@{} adalah Bot Telegram untuk menyimpan Postingan atau File yang dapat Diakses melalui Link Khusus.
+ • Creator: @{}
+ • Framework: <a href='https://docs.pyrogram.org'>Pyrogram</a>
+ • Source Code: <a href='https://github.com/mrismanaziz/File-Sharing-Man'>File-Sharing-Man v4</a>
+👨‍💻 Develoved by </b><a href='https://t.me/Lunatic0de/101'>@Lunatic0de</a>
+"""
