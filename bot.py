79e9209d50659231464a25a7520be1e9f63aff52
VihaanKabir <154652383+VihaanKabir@users.noreply.github.com>
2024-01-21 14:19:15 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import pyromod.listen
 import sys
 
 from pyrogram import Client
+from pyrogram.enums import ParseMode
 
 from config import (
     API_HASH,
@@ -24,6 +25,7 @@ class Bot(Client):
     def __init__(self):
         super().__init__(
             "Bot",
+            name="Bot",
             api_hash=API_HASH,
             api_id=APP_ID,
             plugins={"root": "plugins"},
@@ -139,6 +141,7 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode("html")
+        self.set_parse_mode(ParseMode.HTML)
         self.LOGGER(__name__).info(
             f"[🔥 BERHASIL DIAKTIFKAN! 🔥]"
         )
